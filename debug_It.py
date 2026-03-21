b717af25eed059000f9e53e044b9cbc2f16df07d
Akshit Saxena <akshitsaxena989@gmail.com>
2026-03-21 11:13:06 +0530

    Update debug_It.py

--- debug_It.py
+++ debug_It.py
@@ -1,4 +1,13 @@
 import traceback
+import os
+
+java_paths = [
+    r"C:\Program Files\Common Files\Oracle\Java\javapath",
+    r"C:\Program Files (x86)\Common Files\Oracle\Java\java8path"
+]
+for p in java_paths:
+    if os.path.exists(p) and p.lower() not in os.environ.get('PATH', '').lower():
+        os.environ['PATH'] = p + os.pathsep + os.environ.get('PATH', '')
 
 try:
     import language_tool_python
